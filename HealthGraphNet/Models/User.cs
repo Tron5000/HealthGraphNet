508e39a605e40a1a6300eb756718438415922406
Mark Brady <markdurbinbrady@gmail.com>
2012-08-13 16:05:05 -0700

    Starting to organize project & models.

---
+++ HealthGraphNet/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthGraphNet.Models
+{
+    public class User
+    {
+        public string StrengthTrainingActivities { get; set; }
+        public string Weight { get; set; }
+        public string Settings { get; set; }
+        public string Diabetes { get; set; }
+        public string Team { get; set; }
+        public string Sleep { get; set; }
+        public string FitnessActivities { get; set; }
+        public string UserID { get; set; }
+        public string Nutrition { get; set; }
+        public string GeneralMeasurements { get; set; }
+        public string BackgroundActivities { get; set; }
+        public string Records { get; set; }
+        public string Profile { get; set; }
+    }
+}
